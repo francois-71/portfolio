43672c4a83c07d467714bfbef32445865d45461c
francois-71 <francoisdion71@gmail.com>
2023-11-07 12:39:32 +0100

    testing

--- eportfolio/src/App.js
+++ eportfolio/src/App.js
@@ -35,6 +35,7 @@ function App() {
           <Route path="/projects" element={<Project />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/resume" element={<Resume />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </div>
       <Footer />
